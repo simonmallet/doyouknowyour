d1acf6321f4c3b4186d7b369d07440a76c118d6d
Simon Mallet <simon.mallet@globaleagle.com>
2020-01-17 13:31:44 -0500

    Pokemon answering working + showing next pokemon

--- src/app/play-area/play-area.component.ts
+++ src/app/play-area/play-area.component.ts
@@ -12,13 +12,38 @@ export class PlayAreaComponent implements OnInit {
   private selectedPokemon: number;
   public pokemonImg: string;
   public pokemonChoices = [];
+  public showAnswer = false;
+  private pokemonNames: any;
 
   constructor(private apiService: ApiService) { }
 
   ngOnInit() {
+    this.apiService.getPokemonNames().subscribe((data) => {
+      this.pokemonNames = data;
+      this.setNextPokemon();
+    });
+  }
+
+  private setNextPokemon(): void {
     this.selectedPokemon = this.getRandomPokemonNumber(this.minPokemon, this.maxPokemon);
     this.pokemonImg = '/assets/images/pokemon/' + this.selectedPokemon + '.png';
     this.setPokemonChoices();
+  }
+
+  /**
+   * Selects two wrong pokemon from the pool and shuffles them with the valid answer
+   */
+  private setPokemonChoices(): void {
+    this.pokemonChoices = [];
+
+    for (let i = 0; i < 2; i++) {
+      // @todo: make sure other choices are always unique
+      const wrongPokemon = this.getRandomPokemonNumber(this.minPokemon, this.maxPokemon) - 1;
+      this.pokemonChoices.push({name: this.pokemonNames[wrongPokemon], id: wrongPokemon});
+    }
+    this.pokemonChoices.push({name: this.pokemonNames[this.selectedPokemon - 1], id: this.selectedPokemon});
+    this.pokemonChoices.sort(() => 0.5 - Math.random());
+    console.log(this.pokemonChoices);
   }
 
   /**
@@ -28,24 +53,20 @@ export class PlayAreaComponent implements OnInit {
     return Math.floor(Math.random() * (max - min)) + min;
   }
 
-  private setPokemonChoices(): void {
-    this.apiService.getPokemonNames().subscribe((data) => {
-      for (let i = 0; i < 2; i++) {
-        const wrongPokemon = this.getRandomPokemonNumber(this.minPokemon, this.maxPokemon) - 1;
-        this.pokemonChoices.push({name: data[wrongPokemon], id: wrongPokemon});
-      }
-      this.pokemonChoices.push({name: data[this.selectedPokemon - 1], id: this.selectedPokemon});
-      console.log(this.pokemonChoices);
-    });
-  }
-
   public checkAnswer(pokemonId: number): void {
-    console.log('got here...');
+    this.showAnswer = true;
     if (pokemonId === this.selectedPokemon) {
       console.log('good pokemon index! (good/wrong)', this.selectedPokemon, pokemonId);
+      // @todo: add +1 score & +1 streak
     } else {
       console.log('bad pokemon selection! (good/wrong)', this.selectedPokemon, pokemonId);
+      // @todo: reset streak count to 0
     }
+
+    setTimeout(() => {
+      this.showAnswer = false;
+      this.setNextPokemon();
+    }, 2000);
   }
 
   public showStats() {
